d4c62ba18dabde5fa028a04f76769ff0be34d7b8
omkar <omkar.siras@accenture.com>
2025-10-31 19:30:20 +0530

    corrected template: ChatPromptTemplate

--- 4.LangchainPrompts/chat_prompt_template.py
+++ 4.LangchainPrompts/chat_prompt_template.py
@@ -1,9 +1,8 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.messages import SystemMessage, HumanMessage
 
 chat_template= ChatPromptTemplate([
-    SystemMessage(content='You are a helpful {domain} expert'),
-    HumanMessage(content='Explain in simple terms, what is {topic}')
+    ('system','You are a helpful {domain} expert'),
+    ('human','Explain in simple terms, what is {topic}')
 ])
 
 prompt = chat_template.invoke({'domain': 'cricket', 'topic': 'dusra'})
