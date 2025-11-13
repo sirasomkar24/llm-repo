70c849f83cf78c2c235aa35f0ffb7374bc9fae9b
omkar <omkar.siras@accenture.com>
2025-11-13 11:32:31 +0530

    [ADD] dict and json pydantic output

--- pydantic_Demo.py
+++ pydantic_Demo.py
@@ -5,7 +5,7 @@ class Student(BaseModel):
     name: str = 'Omkar' #set a default value
     age: Optional[int] = None
     email: EmailStr
-    cgpa: float = Field(gt= 0, lt= 10) # Can also set the default parameter in the Field
+    cgpa: float = Field(gt= 0, lt= 10, description='A decimal value representing the CGPA of the student') # Can also set the default parameter in the Field
 
 new_student = {'age': 32,'email': 'abc@gamil.com', 'cgpa': 9.5}
 
@@ -13,4 +13,10 @@ student = Student(**new_student)
 
 # print('type', type(student))
 
-print('output', student)
+print('output', student.model_dump()) # To convert output into the dictionary
+
+# To convert pydantic output into the Json
+
+student_json = student.model_dump_json()
+
+print("json output", student_json)
