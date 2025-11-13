8dfaa56808064add2ff7e9f6f16c15fa5a180fdf
omkar <omkar.siras@accenture.com>
2025-11-13 10:47:42 +0530

    [ADD] pydantic Optional

--- pydantic_Demo.py
+++ pydantic_Demo.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Student(BaseModel):
     name: str = 'Omkar' #set a default value
+    age: Optional[int] = None
 
-new_student = {}
+new_student = {'age': 32}
 
 student = Student(**new_student)
 
-print('type', type(student))
+# print('type', type(student))
 
 print('output', student)
