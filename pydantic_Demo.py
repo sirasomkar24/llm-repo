42b3406e878cb375aab1f0abc19f5dd0ba74b7e5
omkar <omkar.siras@accenture.com>
2025-11-13 11:23:49 +0530

    [FIX] requiremets.txt and added email validation and Field

--- pydantic_Demo.py
+++ pydantic_Demo.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 class Student(BaseModel):
     name: str = 'Omkar' #set a default value
     age: Optional[int] = None
+    email: EmailStr
+    cgpa: float = Field(gt= 0, lt= 10) # Can also set the default parameter in the Field
 
-new_student = {'age': 32}
+new_student = {'age': 32,'email': 'abc@gamil.com', 'cgpa': 9.5}
 
 student = Student(**new_student)
 
