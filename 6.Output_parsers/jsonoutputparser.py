4174e5b28667b2d177bcab19108ed6c7908174d6
omkar <omkar.siras@accenture.com>
2025-11-17 17:46:43 +0530

    [ADD] Json output parser prompt template

---
+++ 6.Output_parsers/jsonoutputparser.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-20b",    
+    task= "text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser= JsonOutputParser()
+
+template = PromptTemplate(
+    template="Give me the name, age and city of the fictional person {format_instruction}",
+    input_variables=[],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+
+)
+
+prompt = template.format()
+
+print(prompt)
